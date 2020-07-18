df5de0c50c821d3c02176825e03abad7ef96d437
Adwi Arifin <adwi@itmi.id>
2020-07-18 23:54:50 +0700

    add store test

---
+++ tests/Feature/Http/Controllers/PersonControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PersonControllerTest extends TestCase
+{
+    use WithFaker;
+
+    /**
+     * @test
+     */
+    public function it_should_store_data()
+    {
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+
+        $response = $this->post(route('person.store'), [
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+        ]);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'id' => true,
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'created_at' => true,
+                'updated_at' => true,
+            ]);
+    }
+}
