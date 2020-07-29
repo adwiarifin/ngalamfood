83d2718a7f5a7d051114afd9375c4f7fc799685a
Adwi Arifin <adwi@itmi.id>
2020-07-29 12:32:17 +0700

    try again

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ import Vuetify from '../plugins/vuetify.js';
 import Routes from './routes.js';
 
 // component File
-import App from './components/app';
+import App from './components/App.vue';
 
 const app = new Vue({
     el: '#app',
